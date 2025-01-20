c3fa270562e7957219080534321716cf34c4fc5a
fdd <kristiyan.ivanov.98@gmail.com>
2025-01-20 09:06:28 +0200

    adding uncommitted changes

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,63 @@
-const Product = () => {
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { Button } from "@mui/material";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+interface ProductProps {
+    product: {
+        url: StaticImageData,
+        name: string,
+        price: number,
+        discount: number,
+        link: string
+    },
+    i: number
+}
+
+const Product = ({ product, i }: ProductProps) => {
     return (
-        <div>
+        <div 
+            key={product.name}
+            className="w-[14.375rem] ps-2 relative"
+        >
+            <div className="relative border border-gray-400 p-4 flex flex-col gap-y-2 hover:scale-105 transition duration-300 ease-in-out">
+                {product.discount !== 0 &&
+                    <div className='absolute top-0 left-0 bg-primary px-1 text-white'>
+                        -{product.discount}%
+                    </div>
+                }
+                <Link href={`/katalog/plocha/${product.link}`} className='border border-gray-100'>
+                    <Image
+                        src={product.url}
+                        alt={product.name}
+                        draggable="false"
+                    />
+                </Link>
+                <Link href=''>
+                    <p className="font-medium hover:text-gray-600">
+                        {product.name}
+                    </p>
+                </Link>
+                {product.discount ?
+                    <p>
+                        <span className="text-gray-400 line-through me-2">
+                            {product.price.toFixed(2)} лв.
+                        </span>
+                        <span className="text-primary me-2">
+                            {(product.price - (product.price * product.discount/100)).toFixed(2)} лв.
+                        </span>
+                    </p>
+                    :
+                    <p className="text-primary">{product.price} лв.</p>
+                }
+                <Button 
+                    variant="contained" 
+                    sx={{ width: '100%' , display: 'block', marginX: 'auto', backgroundColor: 'var(--color-primary)' }}
+                    className='addToCart-btn'
+                >
+                    <ShoppingCartIcon />
+                </Button>
+            </div>
         </div>
     );
 };
