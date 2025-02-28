52e4b6e0e00c77fb2647c1110c16ec255658a246
fdd <kristiyan.ivanov.98@gmail.com>
2025-02-28 13:12:12 +0200

    for some reason previous add didn't work?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '164.90.174.87',
+                port: '',
+                pathname: '/images/**',
+                search: '',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
