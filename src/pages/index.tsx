2cd4e84bd95422b7728c963fe12f4f2ca08225da
fdd <kristiyan.ivanov.98@gmail.com>
2025-08-08 19:18:14 +0300

    no changes, just index page got formatted to 2 spaces

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,59 +7,58 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 
 const Home = () => {
-    const [sliderData, setSliderData] = useState(null);
-    const [popularCategories, setPopularCategories] = useState(null);
-    const [popularProducts, setPopularProducts] = useState(null);
-    const [loading, setLoading] = useState<boolean>(true);
+  const [sliderData, setSliderData] = useState(null);
+  const [popularCategories, setPopularCategories] = useState(null);
+  const [popularProducts, setPopularProducts] = useState(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const [sliderRes, categoriesRes, productsRes] = await Promise.all([
-                fetch("/api/slider"),
-                fetch("/api/categories/popular"),
-                fetch("/api/products/popular"),
-            ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const [sliderRes, categoriesRes, productsRes] = await Promise.all([
+        fetch("/api/slider"),
+        fetch("/api/categories/popular"),
+        fetch("/api/products/popular"),
+      ]);
 
-            if (!sliderRes.ok || !categoriesRes.ok || !productsRes.ok) {
-                throw new Error("One or more requests failed");
-            }
+      if (!sliderRes.ok || !categoriesRes.ok || !productsRes.ok) {
+        throw new Error("One or more requests failed");
+      }
 
-            const [sliderData, categoriesData, productsData] =
-                await Promise.all([
-                    sliderRes.json(),
-                    categoriesRes.json(),
-                    productsRes.json(),
-                ]);
+      const [sliderData, categoriesData, productsData] = await Promise.all([
+        sliderRes.json(),
+        categoriesRes.json(),
+        productsRes.json(),
+      ]);
 
-            setSliderData(sliderData);
-            setPopularCategories(categoriesData);
-            setPopularProducts(productsData);
-            setLoading(false);
-        };
+      setSliderData(sliderData);
+      setPopularCategories(categoriesData);
+      setPopularProducts(productsData);
+      setLoading(false);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    if (loading || !sliderData || !popularCategories || !popularProducts) {
-        return <div className="text-center mt-20">Зареждане...</div>;
-    }
+  if (loading || !sliderData || !popularCategories || !popularProducts) {
+    return <div className="text-center mt-20">Зареждане...</div>;
+  }
 
-    return (
-        <>
-            <Head>
-                <title>Фитнес Настилки</title>
-            </Head>
-            <div className="max-w-full">
-                <Navbar />
-                <div className="w-full aspect-[10/7] sm:aspect-[10/4] lg:aspect-[10/3] my-0 mx-auto">
-                    <Slider slides={sliderData} />
-                </div>
-                <PopularCategories categories={popularCategories} />
-                <PopularProducts products={popularProducts} />
-                <Footer />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Фитнес Настилки</title>
+      </Head>
+      <div className="max-w-full">
+        <Navbar />
+        <div className="w-full aspect-[10/7] sm:aspect-[10/4] lg:aspect-[10/3] my-0 mx-auto">
+          <Slider slides={sliderData} />
+        </div>
+        <PopularCategories categories={popularCategories} />
+        <PopularProducts products={popularProducts} />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Home;
